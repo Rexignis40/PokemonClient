32925859d066b1ca1d7bc9c046112913b3535fde
Rexignis40 <61108765+Rexignis40@users.noreply.github.com>
2022-12-09 15:01:01 +0100

    Home init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ function Home() {
     return (
       <>
       <Header />
+      <img src="https://www.pokepedia.fr/images/a/ab/Sprite_194_2_XY.png?20170628164058" alt="Axoloto gif"/>
+      <img src="https://www.pokepedia.fr/images/3/36/Sprite_037_XY.png?20141211152652" alt="Goupix gif"/>
+      <img src="https://www.pokepedia.fr/images/d/d3/Sprite_548_XY.png?20140404232603" alt="Axoloto gif"/>
       <p>Home</p>
       <object type="audio/ogg" data="../src/img/pokemon.mp3">
       <param name="autostart" value="true" />
