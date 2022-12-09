a79563dca0fc19ef2bccfe28827860951ea8b9b7
ShulingK <timotheimbert3@gmail.com>
2022-12-09 14:56:13 +0100

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <>
+    <nav>
+        <ul>
+            <li><a href="https://www.youtube.com">youtube</a></li>
+            <li><a href="https://www.twitter.com">twitter</a></li>
+            <li><a href="https://www.instagram.com">instagram</a></li>
+            <li><a href="https://www.tinder.com">tinder</a></li>
+        </ul>
+    </nav>
+    </>
+  );
+}
+
+export default Footer;
+
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
 
 function Home() {
     return (
       <>
       <Header />
       <p>Home</p>
+      <object type="audio/ogg" data="../src/img/pokemon.mp3">
+      <param name="autostart" value="true" />
+      <param name="loop" value="true" />
+      </object>
+      <Footer />
       </>
     );
   }
-  
+
   export default Home;
-  
