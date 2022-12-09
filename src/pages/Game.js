235d19eea1a22992e32eb0fd1475f3b2f5dbde2d
Rexignis40 <61108765+Rexignis40@users.noreply.github.com>
2022-12-09 16:54:36 +0100

    Map procedural

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,31 +1,85 @@
 import "./Game.css";
 import Header from "../components/Header.js";
 import React, { useState } from "react";
+import { createNoise2D } from 'simplex-noise';
+
+const perlin = createNoise2D();
+let startX = 10;
+let startY = 10;
+const grass = Array.from(Array(20), () => new Array(20).fill(""));
+grass.map((row, i) =>{
+  row.map((elm, j) =>{
+    console.log(perlin(i, j));
+    let t = Math.floor(Math.abs(perlin(i, j)) * 3) + 1;
+    console.log(t);
+    switch(t){
+      case 1:
+      grass[i][j] = {
+        sprite: <img src="./img/Hautes_herbes.png" alt=""/>,
+        type: 1
+      }
+      break;
+      case 2:
+      grass[i][j] = {
+        sprite: <img src="./img/Water.png" alt=""/>,
+        type: 2
+      }
+      break;
+      case 3:
+      grass[i][j] = {
+        sprite: <img src="./img/Rock.webp" alt=""/>,
+        type: 3
+      }
+      break;
+      default:
+        break;
+    }
+    grass[i][j]["hasPlayeur"] = false;
+  });
+});
 
 function Game() {
-  const [pos, setPos] = useState([10, 10]);
-  const grass = Array.from(Array(20), () => new Array(20).fill(<img src="./img/Hautes_herbes.png" alt=""/>));
-  grass[pos[0]][pos[1]] = <img src="./img/perso/Idle (1).png" alt=""/>;
+  const [pos, setPos] = useState([startX, startY]);
+  grass[pos[0]][pos[1]] = {
+    sprite: <img src="./img/Hautes_herbes.png" alt=""/>,
+    type: 1,
+    hasPlayeur: true
+  };
     
 
     function handlePos(dir){
+      grass[pos[0]][pos[1]]["hasPlayeur"] = false;
       switch(dir){
         case 'z':
-          if(pos[0] - 1 < 1) break;
+          if(pos[0] - 1 < 1 || grass[pos[0] - 1][pos[1]]["type"] !== 1){
+            grass[pos[0]][pos[1]]["hasPlayeur"] = true;
+            break;
+          }
           setPos([pos[0] - 1, pos[1]]);
           break;
         case 's':
-          if(pos[0] + 1 > 18) break;
+          if(pos[0] + 1 > 18 || grass[pos[0] + 1][pos[1]]["type"] !== 1){
+            grass[pos[0]][pos[1]]["hasPlayeur"] = true;
+            break;
+          }
           setPos([pos[0] + 1, pos[1]]);
           break;
         case 'q':
-          if(pos[1] - 1 < 1) break;
+          if(pos[1] - 1 < 1 || grass[pos[0]][pos[1] - 1]["type"] !== 1){
+            grass[pos[0]][pos[1]]["hasPlayeur"] = true;
+            break;
+          }
           setPos([pos[0], pos[1] - 1]);
           break;
         case 'd':
-          if(pos[1] + 1 > 18) break;
+          if(pos[1] + 1 > 18 || grass[pos[0]][pos[1] + 1]["type"] !== 1){
+            grass[pos[0]][pos[1]]["hasPlayeur"] = true;
+            break;
+          }
           setPos([pos[0], pos[1] + 1]);
           break;
+          default:
+            break;
       }
     }
 
@@ -38,7 +92,10 @@ function Game() {
           return(
             <div key={j} className="grassRow">
           {row.map((tile, i) =>{
-            return (<div key={i}>{tile}</div>);
+            if(tile["hasPlayeur"]){
+              return (<div key={i}><img src="./img/Perso/Idle (1).png" alt=""/></div>);
+            }
+            return (<div key={i}>{tile["sprite"]}</div>);
           })}
           </div>);
         })}
