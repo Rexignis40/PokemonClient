de62d76cbc67c2a6737c59ffa94f656ae801ac35
ShulingK <timotheimbert3@gmail.com>
2022-12-13 12:32:36 +0100

    jsp c kwa

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,6 +1,9 @@
-import Header from "../components/Header.js";
 import React, { useEffect, useState } from "react";
 import {getPokemonByName} from "../api/getPokemons.js";
+import './Pokedex.css';
+import Header from "../components/Header.js";
+
+
 
 function Pokedex() {
   const [search, setSearch] = useState("");
@@ -12,24 +15,26 @@ function Pokedex() {
       .then(result => setPokemons(result))
       .catch(error=>console.error("Erreur avec notre API :",error.message));
   },[search]);
+  
 
     return (
       <>
-      <p>Pokedex</p>
+      <Header />
+      <h2>Pokedex</h2>
       <input value={search} onChange={e => setSearch(e.target.value)}/>
-      <div>
+      <div className="pokedex">
       {pokemons.map((p) => {
         return(
-        <div key={p._id}>
-          <p>{p.name}</p>
+        <div key={p._id} className="pokedex-info">
+          <h3>{p.name}</h3>
           <p>{p.genera}</p>
           <img src={p.sprites["front_default"]} alt="" />
+          <p>{p.num}</p>
         </div>
       )})}
-    </div>
+      </div>
       </>
     );
   }
   
-  export default Pokedex;
-  
+export default Pokedex;
